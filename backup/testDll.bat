7d3ccf13b459fe65b45299c4c8afaec774f170df
unknown <michael.j.lukiman@gmail.com>
2019-05-13 00:29:09 -0400

    Reorganized folders.

--- backup/testDll.bat
+++
@@ -1,4 +0,0 @@
-START "WK" "C:\Users\swelly-mapper\AppData\Roaming\Microsoft Games\Age of Empires ii\Age2_x1\WK.exe" NOSOUND
-timeout /t 1
-:: The path to the dll HAS to be an absolute path.
-dist\injectAllTheThings_32.exe -t 1 WK.exe  C:\Users\swelly-mapper\functions\aoc-state-reader\dist\aoc-state-reader-ALLOWLIVE.dll
