a63ada93057e55712530cdbac4f39ae0cd174f61
Ashkara Vingh II <michael.j.lukiman@gmail.com>
2019-05-14 17:35:35 -0400

    Vis

--- vis_tools/graph_val_loss.py
+++ vis_tools/graph_val_loss.py
@@ -8,15 +8,31 @@ with open(val_loss_path,"r") as rdr:
     for line in rdr:
         curr_epoch, curr_loss = line.split(':')
         epochs_list.append(int(curr_epoch))
-        loss_list.append(int(curr_loss))
-
+        loss_list.append(float(curr_loss))
 epochs = np.array(epochs_list)
 loss = np.array(loss_list)
 
-plt.figure(1)
-plt.subplot(211)
+fig = plt.figure(1)
+graphA = plt.subplot(211)
+graphA.set_ylabel('L1 Loss')
 plt.plot(epochs, loss, 'bo', epochs, loss, 'k')
 
-plt.subplot(212)
-plt.plot(epochs, np.cos(2*np.pi*epochs), 'r--')
+epochs_list = []
+loss_list = []
+val_loss_path = sys.argv[2]
+with open(val_loss_path,"r") as rdr:
+    for line in rdr:
+        curr_epoch, curr_loss = line.split(':')
+        epochs_list.append(int(curr_epoch))
+        loss_list.append(float(curr_loss))
+epochs = np.array(epochs_list)
+loss = np.array(loss_list)
+
+epochs = np.array(epochs_list)
+loss = np.array(loss_list)
+graphB = plt.subplot(212)
+graphB.set_ylabel('L2 Loss')
+graphB.set_xlabel('Epochs')
+plt.plot(epochs, loss, 'bo', epochs, loss, 'k')
+
 plt.show()
