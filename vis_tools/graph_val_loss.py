b58d271f375710da7e5b016a8dc908a67b18b6c4
Ashkara Vingh II <michael.j.lukiman@gmail.com>
2019-05-15 13:46:33 -0400

    Graph vis

--- vis_tools/graph_val_loss.py
+++ vis_tools/graph_val_loss.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import sys
+
+hfont = {'fontname':'Monospace', 'size' : 9}
+plt.xticks(fontsize=7, rotation=90)
+# TOP LEFT
 epochs_list = []
 loss_list = []
 val_loss_path = sys.argv[1]
@@ -13,10 +17,12 @@ epochs = np.array(epochs_list)
 loss = np.array(loss_list)
 
 fig = plt.figure(1)
-graphA = plt.subplot(211)
-graphA.set_ylabel('L1 Loss')
-plt.plot(epochs, loss, 'bo', epochs, loss, 'k')
+graphA = plt.subplot(221)
+graphA.set_ylabel('L2 8', **hfont)
+graphA.tick_params(axis = 'both', which = 'major', labelsize = 8)
+plt.plot(epochs, loss, 'r.', epochs, loss, 'k')
 
+# TOP RIGHT
 epochs_list = []
 loss_list = []
 val_loss_path = sys.argv[2]
@@ -28,11 +34,48 @@ with open(val_loss_path,"r") as rdr:
 epochs = np.array(epochs_list)
 loss = np.array(loss_list)
 
+fig = plt.figure(1)
+graphB = plt.subplot(222)
+graphB.set_ylabel('L1 8', **hfont)
+graphB.tick_params(axis = 'both', which = 'major', labelsize = 8)
+plt.plot(epochs, loss, 'y.', epochs, loss, 'k')
+
+# BOTTOM LEFT
+epochs_list = []
+loss_list = []
+val_loss_path = sys.argv[3]
+with open(val_loss_path,"r") as rdr:
+    for line in rdr:
+        curr_epoch, curr_loss = line.split(':')
+        epochs_list.append(int(curr_epoch))
+        loss_list.append(float(curr_loss))
 epochs = np.array(epochs_list)
 loss = np.array(loss_list)
-graphB = plt.subplot(212)
-graphB.set_ylabel('L2 Loss')
-graphB.set_xlabel('Epochs')
-plt.plot(epochs, loss, 'bo', epochs, loss, 'k')
+
+fig = plt.figure(1)
+graphC = plt.subplot(223)
+graphC.set_ylabel('L2 128', **hfont)
+graphC.tick_params(axis = 'both', which = 'major', labelsize = 8)
+plt.plot(epochs, loss, 'r.', epochs, loss, 'k')
+
+# BOTTOM RIGHT
+epochs_list = []
+loss_list = []
+val_loss_path = sys.argv[4]
+with open(val_loss_path,"r") as rdr:
+    for line in rdr:
+        curr_epoch, curr_loss = line.split(':')
+        epochs_list.append(int(curr_epoch))
+        loss_list.append(float(curr_loss))
+epochs = np.array(epochs_list)
+loss = np.array(loss_list)
+
+epochs = np.array(epochs_list)
+loss = np.array(loss_list)
+graphD = plt.subplot(224)
+graphD.set_ylabel('L1 128', **hfont)
+graphD.set_xlabel('Epochs', **hfont)
+graphD.tick_params(axis = 'both', which = 'major', labelsize = 8)
+plt.plot(epochs, loss, 'y.', epochs, loss, 'k')
 
 plt.show()
